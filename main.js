8f99593cf10a73463d01a2f8b5282a2ea3766f67
unknown <jk598200167@gmail.com>
2023-03-03 19:52:19 +0900

    2023-03-03

--- main.js
+++ main.js
@@ -8,18 +8,18 @@ app.get('/',(req,res)=>{
     res.send(`<h2>Made By. MiniCastle</h2>`);
 });
 app.get('/Kobis/movie/search',(req,res)=>{
-    axios.get(`${Kobis.SearchURL}${Kobis.key}&movieNm=${req.query.movieNm}`).then((response)=>{
+    axios.get(`${Kobis.SearchURL}${Kobis.Key}&movieNm=${req.query.movieNm}`).then((response)=>{
         res.send(response.data.movieListResult);
     });
 });
 app.get('/Kobis/movie/info',(req,res)=>{
     
-    axios.get(`${Kobis.InfoURL}${Kobis.key}&movieCd=${req.query.movieCd}`).then((response)=>{
+    axios.get(`${Kobis.InfoURL}${Kobis.Key}&movieCd=${req.query.movieCd}`).then((response)=>{
         res.send(response.data);
     });
 });
 app.get('/Kobis/movie/daily',(req,res)=>{
-    axios.get(`${Kobis.DailyURL}${Kobis.key}&targetDt=${req.query.targetDt}`).then((response)=>{
+    axios.get(`${Kobis.DailyURL}${Kobis.Key}&targetDt=${req.query.targetDt}`).then((response)=>{
         res.send(response.data.boxOfficeResult.dailyBoxOfficeList);
     });
 });
