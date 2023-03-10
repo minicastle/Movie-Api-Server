b85bb138e4a4607c860d0f60efb7c94aabcc58f9
unknown <jk598200167@gmail.com>
2023-03-10 18:28:33 +0900

    2023-03-10

--- main.js
+++ main.js
@@ -22,7 +22,20 @@ app.get('/Kobis/movie/info',(req,res)=>{
 });
 app.get('/Kobis/movie/daily',(req,res)=>{
     axios.get(`${Kobis.DailyURL}${Kobis.Key}&targetDt=${req.query.targetDt}`).then((response)=>{
-        res.send(response.data.boxOfficeResult.dailyBoxOfficeList);
+        let valueData = response.data.boxOfficeResult.dailyBoxOfficeList;
+        const option = {
+            'X-Naver-Client-Id': Naver.id,
+            'X-Naver-Client-Secret': Naver.key
+        };
+        for(let i = 0;i<valueData.length;i++){
+            axios.get(`${Naver.URL}&query=${'영화 '+valueData[i].movieNm+' 포스터'}`,{headers:option})
+            .then((value)=>{
+                valueData[i].poster = value.data.items[0].link
+            });
+        }
+        setTimeout(()=>{
+            res.send(valueData);
+        },3000)
     });
 });
 app.get('/KMDB/movie/info',(req,res)=>{
@@ -37,7 +50,6 @@ app.get('/Naver/movie/poster',(req,res)=>{
     }
     axios.get(`${Naver.URL}&query=${'영화 '+req.query.title+' 포스터'}`,{headers:option})
     .then((value)=>{
-        console.log(value.data.items[0].link);
         res.send(value.data.items[0].link);
     });
 });
