6406c0ad7932305afe18806a9a34dec6cc808abf
unknown <jk598200167@gmail.com>
2023-03-19 15:17:13 +0900

    2023-03-19

--- main.js
+++ main.js
@@ -1,32 +1,34 @@
 const { default: axios } = require('axios');
 const express = require('express');
-const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT||3000;
 const Kobis = require('./Kobis.json');
 const KMDB = require('./KMDB.json');
 const Naver = require('./Naver.json');
 
-const corsOptions = {
-    origin: '*'
-}
-app.use(cors(corsOptions));
 
 app.get('/',(req,res)=>{
+    res.setHeader('Access-Control-Allow-origin', '*');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.send(`<h2>Made By. MiniCastle</h2>`);
 });
 app.get('/Kobis/movie/search',(req,res)=>{
+    res.setHeader('Access-Control-Allow-origin', '*');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
     axios.get(`${Kobis.SearchURL}${Kobis.Key}&movieNm=${req.query.movieNm}&itemPerPage=100`).then((response)=>{
         res.send(response.data.movieListResult);
     });
 });
 app.get('/Kobis/movie/info',(req,res)=>{
-    
+    res.setHeader('Access-Control-Allow-origin', '*');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
     axios.get(`${Kobis.InfoURL}${Kobis.Key}&movieCd=${req.query.movieCd}`).then((response)=>{
         res.send(response.data);
     });
 });
 app.get('/Kobis/movie/daily',(req,res)=>{
+    res.setHeader('Access-Control-Allow-origin', '*');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
     axios.get(`${Kobis.DailyURL}${Kobis.Key}&targetDt=${req.query.targetDt}`).then((response)=>{
         let valueData = response.data.boxOfficeResult.dailyBoxOfficeList;
         const option = {
@@ -45,11 +47,15 @@ app.get('/Kobis/movie/daily',(req,res)=>{
     });
 });
 app.get('/KMDB/movie/info',(req,res)=>{
+    res.setHeader('Access-Control-Allow-origin', '*');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
     axios.get(`${KMDB.URL}${KMDB.Key}&title=${req.query.title}`).then((response)=>{
         res.send(response.data);
     });
 });
 app.get('/Naver/movie/poster',(req,res)=>{
+    res.setHeader('Access-Control-Allow-origin', '*');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
     const option = {
         'X-Naver-Client-Id': Naver.id,
         'X-Naver-Client-Secret': Naver.key
