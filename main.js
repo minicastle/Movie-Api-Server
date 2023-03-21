59e1151afef6d1a0c7eb12cc6183417b077f1f12
unknown <jk598200167@gmail.com>
2023-03-21 14:21:18 +0900

    2023-03-21

--- main.js
+++ main.js
@@ -5,7 +5,13 @@ const PORT = process.env.PORT||3000;
 const Kobis = require('./Kobis.json');
 const KMDB = require('./KMDB.json');
 const Naver = require('./Naver.json');
+const cors = require('cors');
 
+let corsOptions = {
+  origin: "*", // 출처 허용 옵션
+  credential: true, // 사용자 인증이 필요한 리소스(쿠키 ..등) 접근
+};
+app.use(cors(corsOptions));
 
 app.get('/',(req,res)=>{
     res.setHeader('Access-Control-Allow-origin', '*');
