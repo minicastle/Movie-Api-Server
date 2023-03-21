2ab2ef76bca39af198dcb1708d3d3860c3b7f675
unknown <jk598200167@gmail.com>
2023-03-21 14:28:38 +0900

    cors setting all

--- main.js
+++ main.js
@@ -7,11 +7,7 @@ const KMDB = require('./KMDB.json');
 const Naver = require('./Naver.json');
 const cors = require('cors');
 
-let corsOptions = {
-  origin: "*", // 출처 허용 옵션
-  credential: true, // 사용자 인증이 필요한 리소스(쿠키 ..등) 접근
-};
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.get('/',(req,res)=>{
     res.send(`<h2>Made By. MiniCastle</h2>`);
